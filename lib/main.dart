5f37ed19cb0d594eaa4a3768a20c73ec70bae9d2
yayayagi721 <snoopy1995.1.25@gmail.com>
2022-01-24 13:45:35 +0900

    providerの使い方こねくり回し中

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:test_sample/model/address_info.dart';
 import 'package:test_sample/repository/address_repository_impl.dart';
 import 'package:test_sample/view_model/address_list_view_model.dart';
 
 void main() {
-  runApp(const MyWidget(title: "hoge", message: "huga"));
+  runApp(ProviderScope(child: MyWidget()));
 }
 
 final addressRepositoryProvider = Provider.autoDispose(
@@ -16,31 +17,39 @@ final addressListViewModelProvider = StateNotifierProvider.autoDispose(
   (ref) => (AddressListViewModel(ref.read(addressRepositoryProvider))),
 );
 
-class MyWidget extends HookWidget {
-  const MyWidget({
-    Key? key,
-    required this.title,
-    required this.message,
-  }) : super(key: key);
-
-  final String title;
-  final String message;
-
+class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text("hoge"),
         ),
-        body: Center(
-          child: Text(message),
-        ),
+        body: AddressList(),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
         ),
       ),
     );
+  }
+}
+
+class AddressList extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final vm = ref.watch(addressListViewModelProvider);
+
+    return ListView(
+      children: vm.addressList.map((e) => _card(e.address)),
+    );
+  }
+
+  List<Widget> _listContents(List<AddressInfo> addressList) {
+    return;
+  }
+
+  Widget _card(String address) {
+    return ListTile(title: Text(address));
   }
 }
